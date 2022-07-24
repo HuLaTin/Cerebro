5956df056e02a53edcd6d16aa8661e9701b39054
Hunter Tiner <HLTiner@gmail.com>
2022-07-24 17:53:45 -0500

    update to bubble sort

--- randSort.py
+++ randSort.py
@@ -7,8 +7,9 @@ nums =[int(num) for num in nums] # strings to ints
 
 #len(nums)
 numCopy = nums.copy() # make a copy just in case
-done = False # set flag to break while loop
+#done = False # set flag to break while loop
 while True: # infinite while loop
+    swap = False # flag if a swap occured
     for i, j in enumerate(nums[:-1]): # enumerate list of ints
         num1 = nums[i] # set variable num1 int
         num2 = nums[i+1] # set variable num2 int
@@ -16,14 +17,18 @@ while True: # infinite while loop
         if num1 > num2: #compare, greater than
             nums[i] = num2 # if greater than, we swap the elements in the list
             nums[i+1] = num1 # swap
-            break # break the inner for loop
+            #break # break the inner for loop
+            swap = True # set swap to True
 
-        if i == len(nums)-2: # need to remind myself why this works, but it does
-            done = True # set flag as true
-            break # break inner loop
+        #if i == len(nums)-2: # need to remind myself why this works, but it does
+        #    done = True # set flag as true
+        #    break # break inner loop
+
+    if not swap: # if swap flag is not True
+        break # break loop
     
-    if done: # if flag == true
-        break # break while loop
+    #if done: # if flag == true
+    #    break # break while loop
 
 print(nums) # print sorted list to console
 # save?
